97e1a36633e16515207819af25753dd21418e7b7
Paulo <paulosergiosrs08@gmail.com>
2024-09-26 20:35:04 -0300

    upgraded structure

---
+++ src/model/transacao.py
@@ -0,0 +1,10 @@
+from database.sessao import db
+
+class Transacao(db.Model):
+    __tablename__ = 'transacao'
+    id = db.column(db.integer, primary_key =True)
+    conta = db.column(db.String(20), nullable = False)
+    agencia = db.column(db.String(10), nullable = False)
+    texto = db.column(db.String(), nullable = True)
+    valor = db.column(db.Float(), nullable = True)
+
